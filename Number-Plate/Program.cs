27cc10cf9e2b0ab845aae97bd4c3ed19ca31ce5f
yaswanth <ya8wanth@gmail.com>
2023-03-31 15:29:33 +0530

    Add project files.

---
+++ Number-Plate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+class Program
+{
+    static string TransformPlate(string givenString, int number)
+    {
+        string finalString = "";
+
+        givenString = givenString.ToUpper();
+        foreach (char i in givenString)
+        {
+            if (i=='-')
+            {
+                continue;
+            }
+            else
+            {
+                finalString += i;
+            }
+        }
+
+        int position = finalString.Length - number;
+        while (position > 0)
+        {
+            finalString = finalString.Insert(position, "-");
+            position -= number;
+        }
+
+        return finalString.ToString();
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(TransformPlate("5F3Z-2e-9-w", 4));
+        Console.WriteLine(TransformPlate("nlj-206-fv", 3));
+    }
+}
